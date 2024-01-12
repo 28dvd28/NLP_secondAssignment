359a34ddab4bdad114cb435fac30854897dc45d9
28dvd28 <cordiolidavide1@gmail.com>
2024-01-12 02:38:14 +0100

    project completed - mancano commenti

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
-import json
+import os
 from llamaapi import LlamaAPI
 from nltk.tokenize import word_tokenize
+from tqdm import tqdm
 from slice_generator import sliceGenerator
+
+def clear_output_folder():
+    lista_file = os.listdir("output")
+    for file in lista_file:
+        percorso_file = os.path.join("output", file)
+        try:
+            if os.path.isfile(percorso_file):
+                os.remove(percorso_file)
+        except Exception as e:
+            print(f"Errore durante l'eliminazione del file {file}: {e}")
+
+def send_to_LLM(slice_computed):
+    llama = LlamaAPI("LL-EWtbxl8qK56y1VUlIGbgkK1inbLZaTEyh6TuBsZgSdMR1znho1f9pcX22bnvMwJG")
+
+
+    barra = tqdm(total=len(slice_computed), desc='Invio slice a LLama', position=0, leave=False)
+    for j in range(len(slice_computed)):
+
+        slice = slice_computed[j]
+
+        new_file = "slice " + str(slice_computed.index(slice)+1) + ".txt"
+        with open(os.path.join("output", new_file), 'w', encoding='utf-8') as file:
+            file.write(slice)
+
+            input_string = slice
+            # Build the API request
+            api_request_json = {
+                "messages": [
+                    {"role": "user", "content": input_string},
+                ],
+                "stream": False,
+            }
+
+            # Execute the Request
+            try:
+                response = llama.run(api_request_json)
+                response_string = "\n\n\nRESPONSE:\n" + response.json()["choices"][0]["message"]["content"]
+                file.write(response_string)
+            except LlamaAPI.JSONDecodeError:
+                response_string = "\n\n\nRESPONSE:\n" + "error"
+                file.write(response_string)
+
+        barra.update(1)
+
+    barra.close()
 
 if __name__ == "__main__":
 
     with open("input_text_to_slice.txt", 'r', encoding='utf-8') as file:
         text = file.read()
+
+    clear_output_folder()
     
     slice_generator = sliceGenerator(text)
-    slice_computed = list(slice_generator.getSlices().keys())
+    slice_computed = slice_generator.sliced_text
 
-    dimension = [0, len(word_tokenize(text))]
+    print("Computed slices:      ", len(slice_computed))
+    print("Slice tokens total:   ", slice_generator.dimension[0])
+    print("Original text tokens: ", slice_generator.dimension[1])
 
-    for slice in slice_computed:
-        dimension[0] += len(word_tokenize(slice))
+    print("OVERLAPPING PERCENTAGE:")
+    for x in slice_generator.overlapping_percentage:
+        print(f"    - {x}{round(slice_generator.overlapping_percentage[x], 2)}% of their tokens")
+    print()
 
-    print("Dimensione: " + str(dimension))
-
-    # Initialize the SDK
-    llama = LlamaAPI("LL-EWtbxl8qK56y1VUlIGbgkK1inbLZaTEyh6TuBsZgSdMR1znho1f9pcX22bnvMwJG")
-
-    for slice in slice_computed:
-
-        input_string = "Make a summary of the following text:\n" + slice
-        # Build the API request
-        api_request_json = {
-            "messages": [
-                {"role": "user", "content": input_string},
-            ],
-            "stream": False,
-        }
-
-        # Execute the Request
-        response = llama.run(api_request_json)
-        print("RESPONSE: " + response.json()["choices"][0]["message"]["content"])
-        print("\n\n\n")
+    send_to_LLM(slice_computed)
+    
+    print("Slice e rispettive risposte visualizzabili all'interno della cartella output")
--- slice_generator.py
+++ slice_generator.py
@@ -1,10 +1,24 @@
+import sys
+import time
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 import numpy as np 
+import threading
+from tqdm import tqdm
 
-from sklearn.metrics.pairwise import cosine_similarity
+
+
+def rotaing_symbol():
+    global terminate_rotating_symbol
+    symbol = ['|', '/', '-', '\\']
+    i = 0
+    while (not terminate_rotating_symbol):
+        sys.stdout.write('\rSlice_computing: ' + symbol[i%4])
+        sys.stdout.flush()
+        time.sleep(0.1)
+        i += 1
 
 def text_elaboration(text):
 
@@ -44,57 +58,105 @@ def cosine_distance(vector1, vector2):
 def generate_slice(text: str , bow: dict):
 
     text_slices = {}
-
     text_in_sentence = sent_tokenize(text)
+
+    cosine_distance_minimum = 0.8
 
     newslice = ""
     newslice_size = 0
-    for i in range(len(text_in_sentence)):
-        
-        sentence = text_in_sentence[i]
 
-        if newslice_size + len(word_tokenize(sentence)) <= 2000:
-            newslice = newslice + " " + sentence
-            newslice_size += len(word_tokenize(sentence))            
-        else:
-            i -= 1
-            current_slice_vector = vectorize_text(newslice, bow)
-            if len(text_slices) == 0:
-                text_slices[newslice] = current_slice_vector
-                newslice = newslice.replace(sent_tokenize(newslice)[0], '')
-                newslice_size = len(word_tokenize(newslice))
+    while True:
+        for i in range(len(text_in_sentence)):
+            
+            sentence = text_in_sentence[i]
+
+            if newslice_size + len(word_tokenize(sentence)) <= 2000:
+                newslice = newslice + " " + sentence
+                newslice_size += len(word_tokenize(sentence))            
             else:
-                cosine_distances = []
-                for vector in list(text_slices.values()):
-                    cosine_distances.append(cosine_distance(current_slice_vector, vector))
-                
-                if max(cosine_distances)>=0.9:
-                    newslice = newslice.replace(sent_tokenize(newslice)[0], '')
-                    newslice_size = len(word_tokenize(newslice))
-                else:
+                i -= 1
+                current_slice_vector = vectorize_text(newslice, bow)
+                if len(text_slices) == 0:
                     text_slices[newslice] = current_slice_vector
                     newslice = newslice.replace(sent_tokenize(newslice)[0], '')
                     newslice_size = len(word_tokenize(newslice))
+                else:
+                    cosine_distances = []
+                    for vector in list(text_slices.values()):
+                        cosine_distances.append(cosine_distance(current_slice_vector, vector))
+                    
+                    if max(cosine_distances)>=cosine_distance_minimum:
+                        newslice = newslice.replace(sent_tokenize(newslice)[0], '')
+                        newslice_size = len(word_tokenize(newslice))
+                    else:
+                        text_slices[newslice] = current_slice_vector
+                        newslice = newslice.replace(sent_tokenize(newslice)[0], '')
+                        newslice_size = len(word_tokenize(newslice))
 
-    if not (text_in_sentence[len(text_in_sentence)-1] in newslice):
-        newslice = newslice + " " +text_in_sentence[len(text_in_sentence)-1]
+        if not (text_in_sentence[len(text_in_sentence)-1] in newslice):
+            newslice = newslice + " " +text_in_sentence[len(text_in_sentence)-1]
 
 
-    while True:
-        current_slice_vector = vectorize_text(newslice, bow)
-        cosine_distances = []
-        for vector in list(text_slices.values()):
-            cosine_distances.append(cosine_distance(current_slice_vector, vector))
+        while True:
+            current_slice_vector = vectorize_text(newslice, bow)
+            cosine_distances = []
+            for vector in list(text_slices.values()):
+                cosine_distances.append(cosine_distance(current_slice_vector, vector))
+            
+            if max(cosine_distances)>=cosine_distance_minimum or len(word_tokenize(newslice))>2000 :
+                newslice = newslice.replace(sent_tokenize(newslice)[0], '')
+            else:
+                text_slices[newslice] = current_slice_vector
+                break
+
+        # count how many tokens are in total for all the slice generated and in the orginal text
+        # if the amount of tokens in the slice are more or equal than the original text i will terminate
+        dimension = [0, len(word_tokenize(text))]
+        for slice in list(text_slices.keys()):
+            dimension[0] += len(word_tokenize(slice)) 
         
-        if max(cosine_distances)>=0.9 or len(word_tokenize(newslice))>2000 :
-            newslice = newslice.replace(sent_tokenize(newslice)[0], '')
+        if dimension[0] >= dimension[1]:
+            break
         else:
-            text_slices[newslice] = current_slice_vector
-            break     
-    
+            cosine_distance_minimum += 0.05
 
-    return text_slices
+    returnList = list(text_slices.keys())
+    for i in range(len(returnList)):
+        if returnList[i].startswith(" "):
+            returnList[i] =returnList[i][1:]
 
+    return returnList, dimension
+
+
+def overlapping_tokens(sliced_text : list):
+
+    total_overlapping = {}
+
+    for i in range(len(sliced_text)-1):
+
+        current = sent_tokenize(sliced_text[i])
+        next = sent_tokenize(sliced_text[i+1])
+
+        for j in range(2,min([len(next), len(current)])):
+            sub_current = current[len(current)-j+1:]
+            sub_next = next[:j]
+
+            if all(x == y for x, y in zip(sub_current, sub_next)):
+                if not all(x in sub_next for x in current[0:len(current)-j+1]):
+                    s = ""
+                    for string in sub_next:
+                        s = s + string + " "
+                    total_overlapping[f"Slice {i+1} and slice {i+2} overlaps for "]= (200 * len(word_tokenize(s)) / (len(word_tokenize(sliced_text[i])) + len(word_tokenize(sliced_text[i+1]))))
+                    break
+
+    return total_overlapping
+
+
+
+
+
+
+terminate_rotating_symbol = False
 
 class sliceGenerator:
 
@@ -105,8 +167,14 @@ class sliceGenerator:
         
         for word in text_elaboration(self.text):
             self.bow[word] = 0
-        
-        self.sliced_text = generate_slice(self.text, self.bow)
 
-    def getSlices(self):
-        return self.sliced_text
+        thread_rotating_symbol = threading.Thread(target=rotaing_symbol)
+        thread_rotating_symbol.start()
+        self.sliced_text, self.dimension = generate_slice(self.text, self.bow)
+        global terminate_rotating_symbol
+        terminate_rotating_symbol = True
+        thread_rotating_symbol.join()
+
+        print("\r                                            \r", end='')
+
+        self.overlapping_percentage = overlapping_tokens(self.sliced_text)
